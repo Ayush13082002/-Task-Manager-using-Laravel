fc5b8761eba5095fc37da096106ab82ca8ef9e33
Ayush Manoj Bisoyi <75854905+Ayush13082002@users.noreply.github.com>
2023-10-19 01:08:40 +0530

    Add files via upload

---
+++ database/factories/ProductsFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Products>
+ */
+class ProductsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'buyingPrice' => $this->faker->randomFloat(2, 1, 1000),
+            'sellingPrice' => $this->faker->randomFloat(2, 1, 2000),
+            'description' => $this->faker->paragraph,
+            'image_url' => $this->faker->image(public_path('productsImage'), 640, 480, null, false),
+            'weight' => $this->faker->randomFloat(2, 0.1, 50),
+        ];
+    }
+}
